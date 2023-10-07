dc9760c5ad84ee34023804627c1af5182d0b6953
Roman Kachniuk <roman.kachniuk@gmail.com>
2023-10-07 10:22:03 +0200

    added controllers Home&Login, added HTML index&login

--- WebApp/src/main/java/com/isa/webapp/controller/LoginController.java
+++ WebApp/src/main/java/com/isa/webapp/controller/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @GetMapping("index")
-    public String getHomePage () {
-        return "index";
+    @GetMapping("login")
+    public String getLoginPage () {
+        return "login";
     }
 }
