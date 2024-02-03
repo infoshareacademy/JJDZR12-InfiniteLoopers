97ac4d2521147bcc2ad992ea579935ad28a5549e
Szymon Olszewski <olszewskiszym@gmail.com>
2024-02-03 22:05:11 +0100

    Admin role assign and edit credential added. 1st login need fix to force changing data

--- WebApp/src/main/java/com/isa/webapp/controller/AdminController.java
+++ WebApp/src/main/java/com/isa/webapp/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.isa.webapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +19,12 @@ import java.util.stream.Collectors;
 public class AdminController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/manage-users")
@@ -41,21 +44,35 @@ public class AdminController {
     }
 
     @GetMapping("/edit-user/{userId}")
-    public String showEditUserPage(@PathVariable Long userId, Model model) {
+    public String showEditUserPage(@PathVariable Long userId, Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        boolean isAdmin = userDetails.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         User user = userService.getUserById(userId);
         model.addAttribute("user", user);
         return "edit_user";
     }
 
     @PostMapping("/update-user")
-    public String updateUser(@ModelAttribute("user") User updatedUser) {
-        userService.updateUser(updatedUser);
+    public String updateUser(@RequestParam("id") Long id, @ModelAttribute("user") User updatedUser) {
+        User existingUser = userService.getUserById(id);
+        if (existingUser != null) {
+            existingUser.setEmail(updatedUser.getEmail());
+            existingUser.setFirstName(updatedUser.getFirstName());
+            existingUser.setLastName(updatedUser.getLastName());
+            existingUser.setSchoolName(updatedUser.getSchoolName());
+            existingUser.setUserRole(updatedUser.getUserRole());
+            userService.updateUser(existingUser);
+        }
         return "redirect:/admin/manage-users";
     }
 
     @GetMapping("/approve-roles")
-    public String showApproveRolesPage(Model model) {
+    public String showApproveRolesPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         List<User> unapprovedUsers = userService.getUnapprovedUsers();
+        boolean isAdmin = userDetails.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         model.addAttribute("unapprovedUsers", unapprovedUsers);
         return "approve_roles";
     }
@@ -70,6 +87,31 @@ public class AdminController {
             userIds.forEach(userId -> userService.approveUserRoles(userId, userRoles));
         }
         return "redirect:/admin/approve-roles";
+    }
+
+    @GetMapping("/edit-profile")
+    public String showEditProfile(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+        User user = userService.getUserByEmail(userDetails.getUsername());
+        boolean isAdmin = userDetails.getAuthorities().stream()
+                .anyMatch(a -> a.getAuthority().equals("ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
+        model.addAttribute("user", user);
+        return "edit-profile";
+    }
+
+    @PostMapping("/update-profile")
+    public String updateProfile(@ModelAttribute User user, @RequestParam("newPassword") String newPassword, @AuthenticationPrincipal UserDetails userDetails) {
+        User existingUser = userService.getUserByEmail(userDetails.getUsername());
+
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        if (!newPassword.isEmpty()) {
+            existingUser.setPassword(passwordEncoder.encode(newPassword));
+        }
+
+        userService.updateUser(existingUser);
+        return "redirect:/";
     }
 }
 
--- WebApp/src/main/java/com/isa/webapp/security/SetupDataLoader.java
+++ WebApp/src/main/java/com/isa/webapp/security/SetupDataLoader.java
@@ -2,12 +2,10 @@ package com.isa.webapp.security;
 
 import com.isa.webapp.model.User;
 import com.isa.webapp.model.UserRole;
-import com.isa.webapp.service.UserManager;
 import com.isa.webapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 
@@ -17,12 +15,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
+
 
     @Autowired
-    public SetupDataLoader(UserService userService, UserManager userManager, PasswordEncoder passwordEncoder) {
+    public SetupDataLoader(UserService userService) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -37,18 +34,12 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
         User adminUser = new User();
         adminUser.setFirstName("Admin");
         adminUser.setLastName("User");
-        adminUser.setPassword(encodePassword("admin"));
+        adminUser.setPassword(("admin"));
         adminUser.setEmail("admin@example.com");
         adminUser.setUserRole(UserRole.ADMIN);
         adminUser.setApproved(true);
 
-        try {
-            userService.registerUser(adminUser);
-        } catch (Exception e) {
-        }
-    }
+        userService.registerUser(adminUser);
 
-    private String encodePassword(String password) {
-        return passwordEncoder.encode(password);
     }
 }
